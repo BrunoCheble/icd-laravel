130697761b84db50fbf51ceb0d39ede7600f658b
Bruno Cheble <contato.cheble@gmail.com>
2024-05-15 01:15:28 +0100

    Creating route contact page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/contact', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
