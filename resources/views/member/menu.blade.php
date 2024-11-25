f84df7b1d0e5eaa7827952b5c402659b2ce81ed8
Bruno Cheble <contato.cheble@gmail.com>
2024-11-25 22:53:39 +0000

    Atualização no link do whatsapp

--- resources/views/member/menu.blade.php
+++ resources/views/member/menu.blade.php
@@ -43,7 +43,7 @@
             {{ __('Edit') }}
         </x-dropdown-link>
 
-        <x-dropdown-link class="font-bold rounded" target="_blank" :href="'https://wa.me/'.$member->phone_number.'?text=A paz, '.($member->gender == 'M' ? 'irmão ' : 'irmã ').' '.$member->first_name.'. Tudo bem ?'">
+        <x-dropdown-link class="font-bold rounded" target="_blank" :href="'https://wa.me/351'.$member->phone_number.'?text=A paz, '.($member->gender == 'M' ? 'irmão ' : 'irmã ').' '.$member->first_name.'. Tudo bem ?'">
             {{ __('Whatsapp') }}
         </x-dropdown-link>
 
